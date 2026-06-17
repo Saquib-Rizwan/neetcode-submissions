dc642f16ca337e91d42c92d1cbec573030310748
Saquib Rizwan A S <147642522+Saquib-Rizwan@users.noreply.github.com>
2026-06-17 17:01:48 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<String> current=new ArrayList<>();
+        List<List<String>> result=new ArrayList<>();
+        backtrack(s,0,current,result);
+        return result;
+    }
+    public void backtrack(String s,int start,List<String> current,List<List<String>> result)
+    {
+        if(s.length()==start)
+        {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        for(int end=start;end<s.length();end++)
+        {
+            if(isPalindrome(s,start,end))
+            {
+                current.add(s.substring(start,end+1));
+                backtrack(s,end+1,current,result);
+                current.remove(current.size()-1);
+            }
+            
+        }
+    }
+    private boolean isPalindrome(String s,int start,int end)
+    {
+        while(start<end)
+        {
+            if(s.charAt(start)!=s.charAt(end))return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
