be61940488f660691f8629c8342fa889ad5e702e
Saquib Rizwan A S <147642522+Saquib-Rizwan@users.noreply.github.com>
2026-06-24 21:46:59 +0530

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void solve(char[][] board) {
+        int rows=board.length;
+        int cols=board[0].length;
+        boolean[][] safe=new boolean[rows][cols];
+        for(int c=0;c<cols;c++)
+        {
+            if(board[0][c]=='O')
+            {
+                dfs(0,c,board,safe);
+            }
+            if(board[rows-1][c]=='O')
+            {
+                dfs(rows-1,c,board,safe);
+            }
+        }
+        for(int r=0;r<rows;r++)
+        {
+            if(board[r][0]=='O')
+            {
+                dfs(r,0,board,safe);
+            }
+            if(board[r][cols-1]=='O')
+            {
+                dfs(r,cols-1,board,safe);
+            }
+        }
+        for(int r=0;r<rows;r++)
+        {
+            for(int c=0;c<cols;c++)
+            {
+                if(safe[r][c]!=true&&board[r][c]!='X')board[r][c]='X';
+            }
+        }
+    }
+    private void dfs(int r,int c,char[][] board,boolean[][] safe)
+    {
+        if(r>=board.length||r<0||c>=board[0].length||c<0||board[r][c]!='O'||safe[r][c]==true)return;
+        safe[r][c]=true;
+        dfs(r+1,c,board,safe);
+        dfs(r-1,c,board,safe);
+        dfs(r,c+1,board,safe);
+        dfs(r,c-1,board,safe);
+    }
+}
