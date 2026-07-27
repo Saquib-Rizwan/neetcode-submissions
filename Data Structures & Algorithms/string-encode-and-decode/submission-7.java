00fc2e325b218b41c0df6027655cfcc0b4b8c29c
Saquib Rizwan A S <147642522+Saquib-Rizwan@users.noreply.github.com>
2026-07-28 00:05:03 +0530

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb=new StringBuilder("");
+        for(String str:strs)
+        {
+            sb.append(str.length()).append("#").append(str);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        int i=0;
+        List<String> result=new ArrayList<>();
+        while(i<str.length())
+        {
+            int hash=i;
+            while(str.charAt(hash)!='#')
+            {
+                hash++;
+            }
+            int length=Integer.parseInt(str.substring(i,hash));
+            result.add(str.substring(hash+1,hash+1+length));
+            i=hash+1+length;
+        }
+        return result;
+    }
+}
