2c4527eccce2ec7f7dfbed1981cf85f981887f5a
Saquib Rizwan A S <147642522+Saquib-Rizwan@users.noreply.github.com>
2026-08-09 22:05:07 +0530

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int low=0;
+        int high=0;
+        for(char ch:s.toCharArray())
+        {
+            if(ch==')')
+            {
+                low--;
+                high--;
+            }
+            else if(ch=='(')
+            {
+                low++;
+                high++;
+            }
+            else
+            {
+                low--;
+                high++;
+            }
+            low=Math.max(low,0);
+            if(high<0)return false;
+        }
+        return low==0;
+    }
+}
