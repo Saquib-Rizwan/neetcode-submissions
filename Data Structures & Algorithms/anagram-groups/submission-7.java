9f7ccf1c7ca1622a051de8016a79f28871af86b2
Saquib Rizwan A S <147642522+Saquib-Rizwan@users.noreply.github.com>
2026-08-13 16:47:05 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result=new ArrayList<>();
+        HashMap<String,List<String>> map=new HashMap<>();
+        for(String str:strs)
+        {
+            int[] freq=new int[26];
+            for(char ch:str.toCharArray())
+            {
+                freq[ch-'a']++;
+            }
+            String key=Arrays.toString(freq);
+            map.putIfAbsent(key,new ArrayList<>());
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
